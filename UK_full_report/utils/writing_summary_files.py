0ea838315ec61e606131634f3eff825099da2919
VerityHill <verity.hill@ed.ac.uk>
2020-05-13 10:21:45 +0100

    update adm2 plus get singletons

--- UK_full_report/utils/writing_summary_files.py
+++ UK_full_report/utils/writing_summary_files.py
@@ -5,6 +5,21 @@ def write_summary_table(dataframe, output_dir):
 
     dataframe.to_csv(output_dir + "/lineage_summary.tsv", sep="\t")
 
+def write_singletons(intro_alls, output_dir):
+
+
+
+    fw = open(output_dir + "/singletons.csv", 'w')
+    fw.write("UK_lineage,taxon\n")
+
+    for lin in intro_alls:
+        if len(lin.taxa) == 1:
+            l = lin.id + "," + lin.taxa[0].id + "\n"
+
+        fw.write(l)
+    
+    fw.close()
+
 def write_splits_file(splits, output_dir):
 
     fw = open(output_dir + "/split_lineages.csv", 'w')
@@ -27,7 +42,7 @@ def write_omitteds(omitted, output_dir):
     fw.close()
 
 
-def write_summary_files(overall_output_dir, dataframe, omitted, week):
+def write_summary_files(overall_output_dir, dataframe, omitted, week, intro_alls):
 
     output_dir = os.path.join(overall_output_dir, "summary_files_" + week)
 
@@ -38,3 +53,4 @@ def write_summary_files(overall_output_dir, dataframe, omitted, week):
 
     write_summary_table(dataframe, output_dir)
     write_omitteds(omitted, output_dir)
+    write_singletons(intro_alls, output_dir)

3c7293e21e4fbda96addf541c6dfff4d876c69a5
VerityHill <verity.hill@ed.ac.uk>
2020-05-19 14:19:38 +0100

    added full summary file

--- UK_full_report/utils/writing_summary_files.py
+++ UK_full_report/utils/writing_summary_files.py
@@ -5,6 +5,18 @@ def write_summary_table(dataframe, output_dir):
 
     dataframe.to_csv(output_dir + "/lineage_summary.tsv", sep="\t")
 
+def write_all_lines(intro_alls, output_dir):
+
+    fw = open(output_dir + "/all_lineages.csv", 'w')
+    fw.write("Name,oldest_sequence,newest_sequence,number_taxa\n")
+
+    for lin in intro_alls:
+        l = lin.id + "," + str(lin.oldest) + "," + str(lin.mrd) + "," + str(len(lin.taxa)) + "\n"
+
+        fw.write(l)
+    
+    fw.close()
+
 def write_singletons(intro_alls, output_dir):
 
     fw = open(output_dir + "/singletons.csv", 'w')
@@ -52,5 +64,6 @@ def write_summary_files(overall_output_dir, dataframe, omitted, week, intro_alls
     write_summary_table(dataframe, output_dir)
     write_omitteds(omitted, output_dir)
     write_singletons(intro_alls, output_dir)
+    write_all_lins(intro_alls, output_dir)
 
     timeline_data.to_csv(output_dir + "/timeline_raw_data.csv", index=False)

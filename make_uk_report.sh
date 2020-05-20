81951bc03f74a1d34039038aa2777bcc1dbb51dc
VerityHill <verity.hill@ed.ac.uk>
2020-05-20 10:41:48 +0100

    update directory structure for results

--- make_uk_report.sh
+++ make_uk_report.sh
@@ -7,7 +7,6 @@ TREES=$3
 
 
 mkdir UK_full_report/results/$WEEK
-mkdir UK_full_report/results/$WEEK/report_files
 
 if $TREES
 then
@@ -24,7 +23,7 @@ pip install .
 
 echo copying files
 
-scp climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/analysis/5/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv UK_full_report/results/$WEEK/report_files
+scp climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/analysis/5/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv UK_full_report/results/$WEEK/
 
 if $TREES
 then
@@ -35,9 +34,9 @@ echo making md file
  
 if $TREES
 then 
-generate_report --m UK_full_report/results/$WEEK/report_files/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv  --w $WEEK --s UK_report_$WEEK --od UK_full_report/results/$WEEK/report_files/ --i UK_full_report/results/$WEEK/tree_files/
+generate_report --m UK_full_report/results/$WEEK/report_files/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv  --w $WEEK --s UK_report_$WEEK --od UK_full_report/results/$WEEK/ --i UK_full_report/results/$WEEK/tree_files/
 else
-generate_report --m UK_full_report/results/$WEEK/report_files/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv --w $WEEK --s UK_report_$WEEK --od UK_full_report/results/$WEEK/report_files/ 
+generate_report --m UK_full_report/results/$WEEK/report_files/cog_gisaid.lineages.with_all_traits.with_phylotype_traits.csv --w $WEEK --s UK_report_$WEEK --od UK_full_report/results/$WEEK/
 #generate_report --m ~/VirusEvolution\ Dropbox/Group/Coronavirus_projects/UK_project/2020-05-01_rerun2/cog_gisaid.with_all_traits.with_phylotype_traits.fixed_epiweeks.csv --w 2020-05-01 --s UK_report_$WEEK --od UK_full_report/results/2020-05-01/report_files/ 
 fi
 
@@ -59,11 +58,14 @@ rm UK_full_report/results/$WEEK/report_files/cog_gisaid.lineages.with_all_traits
 
 scp UK_report_$WEEK.pdf climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/publish/phylogenetics/reports/
 scp UK_report_$WEEK.md  climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/publish/phylogenetics/reports/
-scp -r UK_full_report/results/$WEEK/report_files climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/publish/phylogenetics/reports/
+scp -r UK_full_report/results/$WEEK/figures_$WEEK climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/publish/phylogenetics/reports/
+scp -r UK_full_report/results/$WEEK/summary_files_$WEEK climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/raccoon-dog/$CLIMBSTEM/publish/phylogenetics/reports/
 
-scp UK_report_$WEEK.pdf climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
-scp UK_report_$WEEK.md climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
-scp -r UK_full_report/results/$WEEK/report_files climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
+
+#scp UK_report_$WEEK.pdf climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
+#scp UK_report_$WEEK.md climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
+#scp -r UK_full_report/results/$WEEK/figures_$WEEK climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
+#scp -r UK_full_report/results/$WEEK/summary_files_$WEEK climb-covid19-hillv@bham.covid19.climb.ac.uk:/cephfs/covid/bham/artifacts/published/latest/phylogenetics/reports/
 
 echo tidying
 
